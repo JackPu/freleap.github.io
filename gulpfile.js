20e1b96e58b6fbdb19bfcd680b8fe4b115e1e44e
JackPu <kakashjack@gmail.com>
2018-07-15 23:49:04 +0800

    add new profile css

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var webserver = require('gulp-webserver');
 gulp.task('webserver', function() {
   gulp.src('')
     .pipe(webserver({
+      host: '0.0.0.0',
       livereload: true,
       directoryListing: true,
       open: true
