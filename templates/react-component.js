e89821dc63b22126a39565f041d0b39ab19ceb80
JackPu <kakashjack@gmail.com>
2019-01-18 14:08:27 +0800

    add template

---
+++ templates/react-component.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+
+import './index.scss';
+
+class AudioItem extends Component {
+  constructor(props) {
+    super(props);
+    // todo
+  }
+
+  componentDidMount() {
+    // todo
+  }
+
+  componentDidUpdate(prevProps) {
+    // todo
+  }
+
+  componentWillUnmount() {
+    // todo
+  }
+
+  render() {
+    // todo
+    return (
+      <div className="audio-item">
+      </div>
+    );
+  }
+}
+
+AudioItem.defaultProps = {
+  prop: 'hello'
+};
+
+export default AudioItem;
